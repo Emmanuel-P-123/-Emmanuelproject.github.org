c0fb3842a36856186e83abd038ee01ca5097b900
Emmanuel-P-123 <121036642+Emmanuel-P-123@users.noreply.github.com>
2023-01-05 08:06:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+.title {
+    font-size: 50px;
+    background-image: linear-gradient(#290066,#4dffff);
+    font-weight: 800;
+    padding: 0 20px;
+        color: white;
+    text-align: center;
+    font-family: 'Staatliches', sans-serif;
+}
+.code{display: flex; justify-content:flex-start;
+}
+.dw{display: flex; justify-content:flex-end ;}
+.colums{display: flex; justify-content:space-around;
+}
